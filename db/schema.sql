8bf4b072e99d9b440476e739eeef59cb4bab6b1e
ebarbosa27 <ebarbosa5449@gmail.com>
2025-08-19 20:55:07 -0500

    created db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
--- TODO
+DROP TABLE IF EXISTS filez;
+DROP TABLE IF EXISTS folders;
+
+CREATE TABLE folders(
+  id serial primary key,
+  name text not null unique
+)
+
+CREATE TABLE files(
+  id serial primary key,
+  name text not null,
+  size int not null,
+  folder_id int references folders(id) ON DELETE CASCADE,
+  UNIQUE (name, folder_id)
+);
