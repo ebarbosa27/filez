6e7d217f44ad3c9a563aa6e97e781d4e95229b29
ebarbosa27 <ebarbosa5449@gmail.com>
2025-08-19 22:37:18 -0500

    seeded data into the db for the files and folder

--- db/schema.sql
+++ db/schema.sql
@@ -1,15 +1,16 @@
-DROP TABLE IF EXISTS filez;
+DROP TABLE IF EXISTS files;
 DROP TABLE IF EXISTS folders;
 
 CREATE TABLE folders(
   id serial primary key,
   name text not null unique
-)
+);
 
 CREATE TABLE files(
   id serial primary key,
   name text not null,
   size int not null,
-  folder_id int references folders(id) ON DELETE CASCADE,
+  folder_id int not null,
+  FOREIGN KEY (folder_id) REFERENCES folders(id) ON DELETE CASCADE,
   UNIQUE (name, folder_id)
 );
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
 
 await db.connect();
 await seed();
@@ -7,4 +8,55 @@ console.log("🌱 Database seeded.");
 
 async function seed() {
   // TODO
+  // Seed at least 5 files to each of the 3 folders
+  // folders: {name: String}
+  // files: {name: String, size: int, folder_id: int}
+
+  for (let i = 0; i < 5; i++) {
+    // Create folder data set and insert it to table
+    const sqlFolder = `INSERT INTO folders(name)
+    VALUES ($1) RETURNING id`;
+
+    let folderError = true;
+    let folderId;
+    while (folderError) {
+      try {
+        const valuesFolder = [faker.color.human()];
+        const { rows: folders } = await db.query(sqlFolder, valuesFolder);
+        folderId = folders[0].id;
+        folderError = false;
+      } catch (err) {
+        if (err.code === "23505") {
+          console.error(`  Error with table "${err.table}": ${err.detail}.`);
+        } else {
+          console.error("Unknown error occured");
+        }
+      }
+    }
+
+    for (let j = 0; j < 8; j++) {
+      // create file data set, link to folder, and insert it to table
+      const sqlFile = `INSERT INTO files(name, size, folder_id)
+        VALUES ($1, $2, $3) `;
+
+      let fileError = true;
+      while (fileError) {
+        try {
+          const valuesFile = [
+            faker.system.commonFileName("txt"),
+            faker.number.int({ min: 10000, max: 9999999 }),
+            folderId,
+          ];
+          await db.query(sqlFile, valuesFile);
+          fileError = false;
+        } catch (err) {
+          if (err.code === "23505") {
+            console.error(`  Error with table "${err.table}": ${err.detail}.`);
+          } else {
+            console.error("Unknown error occured");
+          }
+        }
+      }
+    }
+  }
 }
